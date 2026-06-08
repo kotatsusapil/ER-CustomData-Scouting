d0d354dacc6361cde6e9b60188b4d27c4eb090b8
kotatsusapil <junya.jobs@gmail.com>
2026-06-08 09:31:56 +0900

    FIX: ライブラリの更新に伴うサンプル変更

--- CustomDataScoutingView/Sample/Program.cs
+++ CustomDataScoutingView/Sample/Program.cs
@@ -1,22 +1,31 @@
-﻿Console.WriteLine($"Please select mode{Environment.NewLine}Mode1 :Original read{Environment.NewLine}Mode2 : PlayerStats read");
+﻿using CustomDataScouting;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// 追加しないと言語設定の問題で正常に出力されない可能性がある。
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine($"Please select mode{Environment.NewLine}Mode1 :Original read{Environment.NewLine}Mode2 : PlayerStats read");
 Console.WriteLine("1: Mode1, 2: Mode2, Other: End execution");
 Console.Write("Input: ");
 string mode = Console.ReadLine() ?? "";
 string path = "";
 
 
-if(mode == "1" || mode == "2"){
+if (mode == "1" || mode == "2")
+{
     Console.WriteLine("Please input folder path");
     Console.Write("Input: ");
     path = @"" + Console.ReadLine() ?? "";
 }
 
-if (mode == "1"){
+if (mode == "1")
+{
     Console.WriteLine(readModeOne(path));
     return;
 
 }
-else if(mode == "2")
+else if (mode == "2")
 {
     Console.WriteLine(readModeTwo(path));
     return;
@@ -30,21 +39,16 @@ else
 
 
 
-
-
-
 static String readModeOne(string path)
 {
     CustomDataScouting.CustomDataScouting customDataScouting = new CustomDataScouting.CustomDataScouting(path);
     var result = customDataScouting.ReadCSV();
     string returnText = "";
 
-    foreach(var match in result)
+    foreach (var match in result)
     {
         foreach (var line in match)
-        {
-            returnText += $"Rank: {line.Rank}, Team: {line.TeamName}, Nickname: {line.Nickname}, Character: {line.Character}, Total Field Kill: {line.TotalFieldKill}, Total Score: {line.TournamentTotalScore}{Environment.NewLine}";
-        }
+            returnText += $"Rank: {line.Rank}, Team: {line.TeamName}, Nickname: {line.Nickname}, Character: {line.Subject}, Total Field Kill: {line.TotalFieldKill}, Total Score: {line.TournamentTotalScore}{Environment.NewLine}";
     }
 
     return returnText;
@@ -53,6 +57,19 @@ static String readModeOne(string path)
 
 static String readModeTwo(string path)
 {
-    return "";
+    CustomDataScouting.CustomDataScouting customDataScouting = new CustomDataScouting.CustomDataScouting(path);
+    var results = customDataScouting.ReadCSV();
+    string returnText = "";
+
+    foreach (var result in results)
+    {
+        IList<PlayerMatchData> matches = PlayerMatchData.CreatePlayerDatas(result);
+        foreach (var match in matches)
+        {
+            returnText += $"{match.ToString()}{Environment.NewLine}";
+        }
+    }
+
+    return returnText;
 
 }
